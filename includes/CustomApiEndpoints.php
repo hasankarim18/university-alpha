928944c6ea85de99a240ba4da0749e8650833e80
hasankarim18 <hallysust@gmail.com>
2026-03-08 21:56:26 +0600

    77-working with multiple post types

--- includes/CustomApiEndpoints.php
+++ includes/CustomApiEndpoints.php
@@ -4,29 +4,77 @@
 function combine_search_data(WP_REST_Request $request)
 {
 
-    $search_term = sanitize_text_field($request->get_param('search'));
-    $search = new WP_Query(array(
-        'post_type' => ['professor', 'post',],
+    $search_term = sanitize_text_field($request->get_param('key'));
+    $page = $request->get_param('page') ? $request->get_param('page') : 1;
+    $main_query = new WP_Query(array(
+        'post_type' => ['professor', 'post', 'page', 'event', 'program', 'campus'],
         'posts_per_page' => -1,
-        's' => $search_term
+        'paged' => $page,
+        's' => $search_term,
+        'orderby' => 'post_type',
+        'order' => 'ASC'
 
     ));
 
-    $search_results = array();
+    $search_results = array(
+        'generalInfo' => [],
+        'professors' => [],
+        'programs' => [],
+        'events' => [],
+        'campuses' => []
+    );
 
-    while ($search->have_posts()) {
-        $search->the_post();
+    while ($main_query->have_posts()) {
+        $main_query->the_post();
 
-        array_push($search_results, array(
-            'title' => get_the_title(),
-            'link' => get_the_permalink(),
-            'post_type' => get_post_type(),
+        if (get_post_type() == 'post' or get_post_type() == 'page') {
+            array_push($search_results['generalInfo'], [
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'type' => get_post_type(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'professor') {
+            $related_programs_obj = get_field('related_programs');
+            $related_programs = [];
+            foreach ($related_programs_obj as $item) {
+                array_push($related_programs, $item->post_title);
+            }
+            array_push($search_results['professors'], [
+                'title' => get_the_title(),
+                'type' => get_post_type(),
+                'permalink' => get_the_permalink(),
+                'related_programs' => $related_programs
+            ]);
+        }
+        if (get_post_type() == 'program') {
+            array_push($search_results['programs'], [
+                'title' => get_the_title(),
+                'type' => get_post_type(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'event') {
+            array_push($search_results['events'], [
+                'title' => get_the_title(),
+                'type' => get_post_type(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
+        if (get_post_type() == 'campus') {
+            array_push($search_results['campuses'], [
+                'title' => get_the_title(),
+                'type' => get_post_type(),
+                'permalink' => get_the_permalink()
+            ]);
+        }
 
-        ));
 
 
+        // $search_results;
     }
-
+    wp_reset_postdata();
 
     return $search_results;
 }
